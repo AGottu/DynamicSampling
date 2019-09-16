e4947dabe7d40b00bf383eb9f258a33924d03a1b
Ananth G <agottumu@uci.edu>
2019-09-16 23:55:09 +0000

    Add new file

---
+++ finetune_multi_dataset.py
@@ -0,0 +1,35 @@
+import os
+import json
+import shutil
+import sys
+from torch import cuda
+
+from allennlp.commands import main
+
+# Use overrides to train on CPU.
+instancesPerEpoch = 10000
+overrides = json.dumps({"dataset_reader": {"lazy": True, "allowed_datasets": sys.argv[1], "instances_per_epoch": instancesPerEpoch}, "iterator": {"instances_per_epoch": instancesPerEpoch}, "trainer": {"cuda_device": 0 if cuda.is_available() else -1, "num_epochs": 5}})
+
+model_archive = '%s/pals/model.tar.gz' % os.getcwd() if len(sys.argv) < 3 else sys.argv[2]
+config_file = '%s/configs/nabert-plus-templated.json' % os.getcwd()
+serialization_dir = '%s/palsFineTune' % os.getcwd() if len(sys.argv) < 4 else sys.argv[3]
+
+# Training will fail if the serialization directory already
+# has stuff in it. If you are running the same training loop
+# over and over again for debugging purposes, it will.
+# Hence we wipe it out in advance.
+# BE VERY CAREFUL NOT TO DO THIS FOR ACTUAL TRAINING!
+shutil.rmtree(serialization_dir, ignore_errors=True)
+
+# Assemble the command into sys.argv
+sys.argv = [
+    "allennlp",  # command name, not used by main
+    "fine-tune",
+    "-m", model_archive,
+    "-c", config_file,
+    "-s", serialization_dir,
+    "--include-package", "drop_bert",
+    "-o", overrides
+]
+
+main()
