fb6a44c737ad0ed161f69c00acacbe159f1f6531
Ananth G <agottumu@uci.edu>
2019-09-17 21:58:50 +0000

    Update evaluate.sh

--- evaluate.sh
+++ evaluate.sh
@@ -3,6 +3,7 @@ pip install gdown
 python $PWD/download_files.py
 unzip -o $PWD/data/all_datasets.zip -d $PWD/data
 python $PWD/download_drop.py
+rm -r /agottumu/results
 mkdir /agottumu/results
 python -u $PWD/evaluate_multi_dataset.py drop $1 $2
 python -u $PWD/evaluate_multi_dataset.py duorc $1 $2
