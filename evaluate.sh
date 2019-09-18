eb9ca3670fb26eccc8636462a654fcbd39b2759c
Ananth G <agottumu@uci.edu>
2019-09-18 03:03:25 +0000

    Update evaluate.sh

--- evaluate.sh
+++ evaluate.sh
@@ -5,12 +5,12 @@ unzip -o $PWD/data/all_datasets.zip -d $PWD/data
 python $PWD/download_drop.py
 rm -r /agottumu/results
 mkdir /agottumu/results
-python -u $PWD/evaluate_multi_dataset.py drop $1 $2
-python -u $PWD/evaluate_multi_dataset.py duorc $1 $2
-python -u $PWD/evaluate_multi_dataset.py narrativeqa $1 $2
-python -u $PWD/evaluate_multi_dataset.py newsqa $1 $2
-python -u $PWD/evaluate_multi_dataset.py quoref $1 $2
-python -u $PWD/evaluate_multi_dataset.py ropes $1 $2
+#python -u $PWD/evaluate_multi_dataset.py drop $1 $2
+#python -u $PWD/evaluate_multi_dataset.py duorc $1 $2
+#python -u $PWD/evaluate_multi_dataset.py narrativeqa $1 $2
+#python -u $PWD/evaluate_multi_dataset.py newsqa $1 $2
+#python -u $PWD/evaluate_multi_dataset.py quoref $1 $2
+#python -u $PWD/evaluate_multi_dataset.py ropes $1 $2
 python -u $PWD/evaluate_multi_dataset.py squad $1 $2
 python -u $PWD/evaluate_multi_dataset.py squad2 $1 $2
 python -u $PWD/evaluate_multi_dataset.py all $1 $2
