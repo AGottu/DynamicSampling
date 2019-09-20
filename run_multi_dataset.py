8ebd5e760c293da0cb612c29c066755b39e11a30
Ananth G <agottumu@uci.edu>
2019-09-20 01:45:29 +0000

    Update run_multi_dataset.py

--- run_multi_dataset.py
+++ run_multi_dataset.py
@@ -8,7 +8,8 @@ from allennlp.commands import main
 
 # Use overrides to train on CPU.
 instancesPerEpoch = 50000 if sys.argv[1] == 'all-sample' else None
-overrides = json.dumps({"dataset_reader": {"lazy": True, "allowed_datasets": sys.argv[1], "instances_per_epoch": instancesPerEpoch}, "iterator": {"instances_per_epoch": instancesPerEpoch}, "trainer": {"cuda_device": 0 if cuda.is_available() else -1, "num_epochs": 10}})
+numEpochs = 10
+overrides = json.dumps({"dataset_reader": {"lazy": True, "allowed_datasets": sys.argv[1], "instances_per_epoch": instancesPerEpoch, "numEpochs": numEpochs}, "iterator": {"instances_per_epoch": instancesPerEpoch}, "trainer": {"cuda_device": 0 if cuda.is_available() else -1, "num_epochs": numEpochs}})
 
 config_file = '%s/configs/nabert-plus-templated.json' % os.getcwd()
 serialization_dir = '%s/pals' % os.getcwd() if len(sys.argv) < 3 else sys.argv[2]
