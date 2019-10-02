7d124d6d1bdc639c77b69870e16936197f6f27b0
Ananth G <agottumu@uci.edu>
2019-10-02 05:45:24 +0000

    Update run_prediction.sh

--- run_prediction.sh
+++ run_prediction.sh
@@ -3,4 +3,4 @@ pip install gdown
 python $PWD/download_files.py
 unzip -o $PWD/data/all_datasets.zip -d $PWD/data
 python $PWD/download_drop.py
-python -u $PWD/run_multi_dataset.py $1 $2
+python -u $PWD/run_multi_dataset.py $1 $2 $3
