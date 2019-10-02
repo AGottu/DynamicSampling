5379e9f377f27a2d791b4ad4e0da0639193f5433
Ananth G <agottumu@uci.edu>
2019-10-02 05:46:53 +0000

    Update run_multi_dataset.py

--- run_multi_dataset.py
+++ run_multi_dataset.py
@@ -6,14 +6,14 @@ from torch import cuda
 
 from allennlp.commands import main
 
-assert len(sys.argv) == 3
+assert len(sys.argv) == 4
 # Use overrides to train on CPU.
 instancesPerEpoch = 50000 if sys.argv[1] == 'all-sample' else None
-numEpochs = 2
+numEpochs = int(sys.argv[3])
 overrides = json.dumps({"dataset_reader": {"lazy": True, "allowed_datasets": sys.argv[1], "instances_per_epoch": instancesPerEpoch, "numEpochs": numEpochs}, "iterator": {"instances_per_epoch": instancesPerEpoch}, "trainer": {"cuda_device": 0 if cuda.is_available() else -1, "num_epochs": numEpochs}})
 
 config_file = '%s/configs/nabert-plus-templated.json' % os.getcwd()
-serialization_dir = '%s/pals' % os.getcwd() if len(sys.argv) < 3 else sys.argv[2]
+serialization_dir = sys.argv[2]
 
 # Training will fail if the serialization directory already
 # has stuff in it. If you are running the same training loop
