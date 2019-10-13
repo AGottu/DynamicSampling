bb410059f3d58209c2c811cda6c63174274a7578
Ananth G <agottumu@uci.edu>
2019-10-13 10:04:54 +0000

    Upload New File

---
+++ drop_bert/dynamic_iterator.py
@@ -0,0 +1,109 @@
+import logging
+from overrides import overrides
+from collections import deque
+from typing import Dict, Union, Iterable, Iterator, List, Optional, Tuple, Deque
+from collections import defaultdict
+import itertools
+from itertools import cycle
+import numpy as np
+import math
+import random
+
+import torch
+
+from allennlp.common.registrable import Registrable
+from allennlp.common.util import is_lazy, lazy_groups_of, ensure_list
+from allennlp.data.dataset import Batch
+from allennlp.data.fields import MetadataField
+from allennlp.data.instance import Instance
+from allennlp.data.vocabulary import Vocabulary
+from allennlp.data.iterators.data_iterator import add_epoch_number
+from allennlp.data.iterators import DataIterator
+from allennlp.data.iterators import BasicIterator
+
+logger = logging.getLogger(__name__)
+
+TensorDict = Dict[str, Union[torch.Tensor, Dict[str, torch.Tensor]]]
+
+DATASETS = ('drop', 'duorc', 'narrativeqa', 'newsqa', 'quoref', 'ropes', 'squad', 'squad2')
+idealDevLosses = {'drop': 1311376.45, 'newsqa': 3287434.267, 'squad2': 850474.152, 'quoref': 872098.2, 'ropes': 585288.3, 'narrativeqa': 2505892.7521, 'squad': 1133777.1, 'duorc': 3664924.6}
+
+@DataIterator.register("dynamic")
+class DynamicIterator(BasicIterator):
+    """
+    An iterator that samples instances from 8 datasets based on individual dataset losses.
+    """
+    def __init__(
+        self,
+        batch_size: int = 32,
+        instances_per_epoch: int = None,
+        max_instances_in_memory: int = None,
+        cache_instances: bool = False,
+        track_epoch: bool = False,
+        maximum_samples_per_batch: Tuple[str, int] = None,
+    ) -> None:
+        super().__init__(batch_size, instances_per_epoch, max_instances_in_memory, cache_instances, track_epoch, maximum_samples_per_batch)
+        self.train_iterators = None
+        self.dev_iterators = None
+        self.train_dataset_numbers = dict()
+
+    def chooseDataset(self, datasetName: str):
+        self.dataset_choice = datasetName
+
+    def dynamic_iterator(self, losses):
+        i = 0
+        datasetNames = []
+        sample_probs = []
+        for datasetName, dev_loss in losses.items():
+            ideal_dev_loss = idealDevLosses[datasetName]
+            loss_gap = max(0.01, dev_loss - ideal_dev_loss)
+            datasetNames.append(datasetName)
+            sample_probs.append(loss_gap)
+        alpha = 0.5 # Square root sampling
+        sample_probs = [p**alpha for p in sample_probs]
+        tot = sum(sample_probs)
+        sample_probs = [p/tot for p in sample_probs]
+        for step in range(self._instances_per_epoch):
+            datasetIndex = np.random.choice(len(sample_probs), p=sample_probs)
+            datasetChosen = datasetNames[datasetIndex]
+            self.train_dataset_numbers[datasetChosen] = self.train_dataset_numbers.get(datasetChosen, 0) + 1
+            if i % 10 == 0:
+                print('\n')
+                print(self.train_dataset_numbers)
+                print('\n')
+            i += 1
+            yield next(self.train_iterators[datasetChosen])
+
+    def full_iterator(self):
+        if self.dataset_choice in DATASETS:
+            curr_iterator = self.dev_iterators[self.dataset_choice]
+            yield from curr_iterator
+        else:
+            assert self.dataset_choice == 'all'
+            for datasetName, curr_iterator in self.dev_iterators.items():
+                yield from curr_iterator
+
+    @overrides
+    def __call__(
+        self, instances: Iterable[Instance], num_epochs: int = None, shuffle: bool = True
+    ) -> Iterator[TensorDict]:
+
+        ### Ananth ###
+        assert num_epochs == 1 # Iterator should only be called from train_epoch or validation_loss, both of which are for 1 epoch
+        instances = list(instances)
+        assert len(instances) == 1
+        inst = instances[0]
+        trainDev = inst.fields["metadata"].metadata["trainDev"]
+        assert trainDev in ('train', 'dev')
+        iterators = inst.fields["metadata"].metadata["iterators"]
+        if trainDev == 'train' and self.train_iterators is None:
+            self.train_iterators = {key:cycle(value) for (key,value) in iterators.items()} # Cycle for training iterators since we're sampling
+        elif trainDev == 'dev' and self.dev_iterators is None:
+            self.dev_iterators = {key:value for (key,value) in iterators.items()} # No cycle for dev iterators since we're traversing whole thing once
+        if trainDev == 'train':
+            losses = inst.fields["metadata"].metadata["val_losses"]
+            new_instances = self.dynamic_iterator(losses)
+        else:
+            new_instances = self.full_iterator()
+        ### Ananth ###
+        yield from super().__call__(new_instances, num_epochs, shuffle)
