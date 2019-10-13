879935bcbde522cea2f2a3f754549116b942dea2
Ananth G <agottumu@uci.edu>
2019-10-13 22:11:40 +0000

    Update finetune_multi_dataset.py

--- finetune_multi_dataset.py
+++ finetune_multi_dataset.py
@@ -7,13 +7,18 @@ from torch import cuda
 from allennlp.commands import main
 
 # Use overrides to train on CPU.
-instancesPerEpoch = 50000 if sys.argv[1] == 'all-sample' else None
+dynamic = sys.argv[1] == 'dynamic'
+instancesPerEpoch = 50000 if dynamic else None
+iteratorType = 'dynamic' if dynamic else 'basic'
 numEpochs = 5
-overrides = json.dumps({"dataset_reader": {"lazy": True, "allowed_datasets": sys.argv[1], "instances_per_epoch": instancesPerEpoch, "numEpochs": numEpochs}, "iterator": {"instances_per_epoch": instancesPerEpoch}, "trainer": {"cuda_device": 0 if cuda.is_available() else -1, "num_epochs": numEpochs}})
+overrides_dict = {"dataset_reader": {"lazy": True, "allowed_datasets": sys.argv[1], "instances_per_epoch": instancesPerEpoch}, "iterator": {"type": iteratorType, "instances_per_epoch": instancesPerEpoch}, "trainer": {"cuda_device": 0 if cuda.is_available() else -1, "num_epochs": numEpochs}}
+if dynamic:
+    overrides_dict["trainer"]["type"] = 'dynamic-sample'
+overrides = json.dumps(overrides_dict)
 
-model_archive = '%s/pals/model.tar.gz' % os.getcwd() if len(sys.argv) < 3 else sys.argv[2]
+model_archive = sys.argv[2]
 config_file = '%s/configs/nabert-plus-templated.json' % os.getcwd()
-serialization_dir = '%s/palsFineTune' % os.getcwd() if len(sys.argv) < 4 else sys.argv[3]
+serialization_dir = sys.argv[3]
 
 # Training will fail if the serialization directory already
 # has stuff in it. If you are running the same training loop
