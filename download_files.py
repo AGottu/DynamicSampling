d1e75a9e1df8f0f1d1c2d5e34c845032d2b134ad
Ananth G <agottumu@uci.edu>
2019-10-23 21:00:58 +0000

    Update download_files.py

--- download_files.py
+++ download_files.py
@@ -3,11 +3,11 @@ import gdown
 
 output1 = 'data/all_datasets.zip'
 if not os.path.isfile(output1):
-    url1 = f'https://drive.google.com/uc?id=1Cq0ibIUqwYCL1Z77gVrzGXWXPx4172JY'
-    #url1 = f'https://drive.google.com/uc?id=1QYfG0mV75RXzi5RkHGedZGItWvJUwRnG'
+    #url1 = f'https://drive.google.com/uc?id=1Cq0ibIUqwYCL1Z77gVrzGXWXPx4172JY'
+    url1 = f'https://drive.google.com/uc?id=1QYfG0mV75RXzi5RkHGedZGItWvJUwRnG'
     gdown.download(url1, output1, quiet=False)
 
 output2 = 'bert/pytorch_model.bin'
 if not os.path.isfile(output2):
     url2 = f'https://drive.google.com/uc?id=1ljo1d5ZQcfAMZBAHhyfNjtkqT2UmzAeB' 
-    gdown.download(url2, output2, quiet=False)
+    #gdown.download(url2, output2, quiet=False)
