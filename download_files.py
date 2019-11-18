2f72fd4b314e2300f9ae432aba6b470096cd0e22
Ananth G <agottumu@uci.edu>
2019-11-18 06:24:11 +0000

    Update download_files.py

--- download_files.py
+++ download_files.py
@@ -3,8 +3,8 @@ import gdown
 
 output1 = 'data/all_datasets.zip'
 if not os.path.isfile(output1):
-    #url1 = f'https://drive.google.com/uc?id=1gSOd0N0QiaEv1E5ipp6zvzXVdmgcdjvn' # New Ropes
-    url1 = f'https://drive.google.com/uc?id=18TaondJzLPEEFqov32EK1A0fDk39bFWE' # Old Ropes
+    url1 = f'https://drive.google.com/uc?id=1gSOd0N0QiaEv1E5ipp6zvzXVdmgcdjvn' # New Ropes
+    #url1 = f'https://drive.google.com/uc?id=18TaondJzLPEEFqov32EK1A0fDk39bFWE' # Old Ropes
     
     
     #url1 = f'https://drive.google.com/uc?id=1Cq0ibIUqwYCL1Z77gVrzGXWXPx4172JY'
