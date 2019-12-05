dd341448d49ab2a84b40c00643564fc2924127f3
Ananth G <agottumu@uci.edu>
2019-12-05 00:24:38 +0000

    Delete download_drop.py

--- download_drop.py
+++
@@ -1,7 +0,0 @@
-import os.path
-import gdown
-
-output1 = 'data/all_datasets/train/train.drop.jsonl'
-if not os.path.isfile(output1):
-    url1 = f'https://drive.google.com/uc?id=1fJcEE_7yV4oUUocyBdkLpmBu_Zkd60iY'    
-    gdown.download(url1, output1, quiet=False)
