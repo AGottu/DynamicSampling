52af0eb5a99d26be7158cb9e13c011e0951841f1
Ananth G <agottumu@uci.edu>
2019-12-07 09:28:56 +0000

    Update augmented_bert.py

--- drop_bert/augmented_bert.py
+++ drop_bert/augmented_bert.py
@@ -15,7 +15,7 @@ import pickle
 
 from drop_bert.nhelpers import tokenlist_to_passage, beam_search, evaluate_postfix
 
-#NO_ANSWER_THRESHOLD = 0.0#207.0
+NO_ANSWER_THRESHOLD = 0.0#207.0
 logger = logging.getLogger(__name__)
 
 @Model.register("augmented_bert")
