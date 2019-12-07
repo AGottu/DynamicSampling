ef5e9ad1c7fdcf223e0ede6cdda2808588898912
Ananth G <agottumu@uci.edu>
2019-12-07 06:46:18 +0000

    Update run_prediction.sh

--- run_prediction.sh
+++ run_prediction.sh
@@ -6,4 +6,4 @@ unzip -o $PWD/data/all_datasets.zip -d $PWD/data
 #python $PWD/download_drop.py
 rm -r /agottumu/results
 mkdir /agottumu/results
-python -u $PWD/run_multi_dataset.py $1 $2 $3
+CUDA_VISIBLE_DEVICES=0,1 python -u $PWD/run_multi_dataset.py $1 $2 $3
